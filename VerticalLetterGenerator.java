d327687568ab0c54a18a8670e472043e3ffabd48
dfbantigue <35215254+dfbantigue@users.noreply.github.com>
2018-03-08 12:27:04 +0800

    Update VerticalLetterGenerator.java

--- VerticalLetterGenerator.java
+++ VerticalLetterGenerator.java
@@ -1,38 +1,38 @@
-package oops;
+package oop;
+
+import common.Constant;
 
 public class VerticalLetterGenerator {
-
 	public void printVertical(String arr[], int scale) throws Exception {
-		if(scale<3||scale%2==0)
-			throw new Exception("Input Value must be greater than or equal to 3, and must be odd numbers");
-		
+		if (scale < Constant.MININUM_ALLOWED_VALUES || scale % Constant.MOD == 0)
+			throw new Exception(Constant.ALLOWED_VALUES_ERR_MSG);
+
 		StringBuffer strText = new StringBuffer();
-		
-		for (int k = 0; k < arr.length; k++) {
-			for (int i = 0, j = scale - 1; i < scale; i++, j--) {
-				
-				try {
-					printMe((Letter) Class.forName("oops." + arr[k]).newInstance(), i, scale, j,strText);
-				} catch (InstantiationException e) {
-					
-				} catch (IllegalAccessException e) {
-					
-				} catch (ClassNotFoundException e) {
-					throw new Exception("This Letter is not yet implemented");
-				}
-				
-				strText.append("\n");
+
+		for (int i = 0, k = 0, j = scale - 1; i < scale * arr.length; i++, j--) {
+
+			if (i != 0 && i % scale == 0) {
+				j = scale - 1;
+				k++;
 			}
-			
+
+			try {
+				/* Calling the appropriate class based on array of String value */
+				printIt((Letter) Class.forName("oop." + arr[k].toUpperCase()).newInstance(), i % scale, scale, j,strText);
+			} catch (ClassNotFoundException e) {
+				throw new Exception(Constant.NOT_IMPLEMENT_LETTER_ERR_MSG);
+			}
+			strText.append(Constant.NEXTLINE);
 		}
-		
+
 		System.out.println(strText.toString());
 
 	}
 
-	private void printMe(Letter letter, int i, int scale, int j,StringBuffer strText) {
+	private void printIt(Letter letter, int i, int scale, int j, StringBuffer strText) {
 		for (int k = 0; k < scale; k++) {
-			letter.printMe(i, scale, j,k,strText);
+			letter.printLetter(i, scale, j, k, strText);
 		}
 	}
 }
+
